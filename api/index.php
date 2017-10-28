1690dddda9682b048659e0d724327837f0e77d00
sterlingrobot <sterlingrobot@gmail.com>
2017-10-28 11:21:05 -0600

    api initial

---
+++ api/index.php
@@ -0,0 +1,25 @@
+<?php
+
+	switch($_SERVER['REQUEST_METHOD']) {
+
+		case 'POST':
+
+			break;
+
+		case 'GET':
+		default:
+
+			break;
+
+	}
+
+	function getRecord($id) {
+
+	}
+
+	function updateRecord($id) {
+		
+	}
+
+
+?>
