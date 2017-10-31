c652f7ae5af4eb1a5f8584a623434a92e644210b
Leo Vroegindewey <leonard.v@protonmail.com>
2017-10-31 08:33:12 -0600

    remove token file

--- api/token.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-	// replace bb25b094ad... with your generated token
-	define('GITHUB_TOKEN', 'ed02e549a54fe05343a62035b4858d7a9702be7e');
-	define('PROXY_SERVER', 'wwww-proxy.us.oracle.com');
