1ac2c476882dd77b41ec0ab382305635df634673
sterlingrobot <sterlingrobot@gmail.com>
2017-12-04 00:21:32 -0700

    table template updates and sorting headings

--- assets/js/render-list-item.js
+++ assets/js/render-list-item.js
@@ -19,13 +19,7 @@ jQuery(document).ready(function($) {
         	return a.rank ? a.rank - b.rank : 1;
         });
 
-			  var list = coins.reduce(function(prev,curr) {
-			        	var html = template(curr);
-			        	return prev + html;
-			        },'');
-
-			  $('#coin-list tbody').html(list);
-
+        renderTableView(coins);
     })
     .fail(function(err) {
     	console.log(err);
@@ -33,7 +27,57 @@ jQuery(document).ready(function($) {
     });
 	};
 
+	var renderTableView = function(coins) {
+
+		  var list = coins.reduce(function(prev,curr) {
+		        	var html = template(curr);
+		        	return prev + html;
+		        },'');
+
+		  $('#coin-list tbody').html(list);
+
+	};
+
+	var onTableHeadClicked = function(e) {
+
+		var $hdg = $(this),
+				prop = $hdg.data('prop'),
+				asc, abs;
+
+		asc = !$hdg.data('asc');
+		$hdg.data('asc', asc);
+
+		abs = asc ? 1 : -1;
+
+		coins.sort(function(a,b) {
+
+			var nA, nB, arr;
+
+			if(prop.split('.')[0] === 'latest') {
+				if(a.data) {
+					arr = Object.keys(a.data).sort().reverse();
+					nA = a.data[arr[0]][prop.split('.')[1]];
+				}
+				if(a.data) {
+					arr = Object.keys(b.data).sort().reverse();
+					nB = b.data[arr[0]][prop.split('.')[1]];
+				}
+			} else {
+				nA = 'string' === typeof a[prop] ? a[prop].toLowerCase() : a[prop];
+				nB = 'string' === typeof b[prop] ? b[prop].toLowerCase() : b[prop];
+			}
+			if(nA === nB) return 0;
+			return nA < nB ? -1 * abs : 1 * abs;
+		});
+
+
+		renderTableView(coins);
+
+	};
+
 	fetchJSONdata();
+
+	$('#coin-list').on('click', 'th', onTableHeadClicked);
 
 	Handlebars.registerHelper('dateFormat', function(d) {
 		var options = { year: 'numeric', month: 'long', day: 'numeric' };
