2f1147158792b0bf21d6ebd4e3da93de4d7c5416
sterlingrobot <sterlingrobot@gmail.com>
2017-12-22 03:15:05 -0700

    api json update fixes, add backup method

--- api/json.php
+++ api/json.php
@@ -5,9 +5,10 @@ define('REMOTE_FILE', 'https://api.myjson.com/bins/909wb');
 define('LOCAL_FILE', dirname($_SERVER['DOCUMENT_ROOT']) . '/assets/json/data.json');
 
 define('JSON_FILE', isset($_REQUEST['local']) ? LOCAL_FILE : REMOTE_FILE);
+define('FIELDS_FILE', dirname($_SERVER['DOCUMENT_ROOT']) . '/assets/json/form-fields.json');
 
-define('DEBUG', isset($_REQUEST['debug']) ? $_REQUEST['debug'] : false);
-define('PROXY', isset($_REQUEST['proxy']) ? $_REQUEST['proxy'] : false);
+define('DEBUG', isset($_REQUEST['debug']));
+define('PROXY', isset($_REQUEST['proxy']));
 
 $json = null;
 
@@ -26,33 +27,48 @@ function fetchJSON($url=JSON_FILE) {
 		$context = stream_context_create($c);
 	}
 
-	$json = sortJSON(json_decode(file_get_contents($url, false, $context)));
-
-	if(json_last_error() !== JSON_ERROR_NONE) {
-		throw new Exception('Error reading ' . $url . ' : ' . json_last_error_msg());
+	try {
+		$data = @file_get_contents($url, false, $context);
+		if(!$data || $data->Response === 'Error') {
+			errorLog('FETCH_JSON_ERROR',
+				isset($data->Message) ? $data->Message : 'Possible network error.  You may need to add the <pre style="display:inline;padding:5px;background:#fff">?proxy</pre> flag to your url if you are on VPN.');
+		}
+		$return = sortJSON(json_decode($data));
+		if($url === JSON_FILE) {
+			$json = $return;
+		}
+		if(json_last_error() !== JSON_ERROR_NONE) {
+			errorLog('FETCH_JSON_ERROR','Error reading ' . $url . ' : ' . json_last_error_msg());
+		}
+	} catch (Exception $e) {
+		errorLog('FETCH_JSON_ERROR', $e->getMessage());
 	}
 
-	return $json;
+	return $return;
+
 }
 
 function getRecord($obj) {
 	global $json;
 
 	if($json === null) fetchJSON();
+	if(errorOutput()->errors) return errorOutput();
 
 	extract($obj);
 
+	if(isset($index)) {
+		return $json[$index];
+	}
+	if(isset($symbol)) {
+		return getRecordBySymbol($symbol);
+	}
 	if(isset($id)) {
 		return getRecordById($id);
-	} else if(isset($index)) {
-		return $json[$index];
-	} else if(isset($owner) || isset($name)) {
-		return getRecordByName($owner, $name);
-	} else if(isset($symbol)) {
-		return getRecordBySymbol($symbol);
-	} else {
-		return $json;
 	}
+	if(isset($owner) || isset($name)) {
+		return getRecordByName($owner, $name);
+	}
+	return $json;
 
 }
 
@@ -89,6 +105,7 @@ function addRecords($records) {
 	global $json;
 
 	if($json === null) fetchJSON();
+	if(errorOutput()->errors) return errorOutput();
 
 	$added = array();
 
@@ -122,20 +139,36 @@ function updateRecords($records) {
 	global $json;
 
 	if($json === null) fetchJSON();
+	if(errorOutput()->errors) return errorOutput();
+
+	$fields = array_map(function($field) {
+							return $field->name;
+						}, fetchJSON(FIELDS_FILE));
 
 	$updated = array();
 
 	foreach($records as $record) {
-		$idx = array_search($record->id, array_map(function($coin) { return $coin->id; }, $json));
+
+		$idx = array_search($record->symbol, array_map(function($coin) { return $coin->symbol; }, $json));
+
 		$record->languages = is_array($record->languages) ? $record->languages : explode(',', $record->languages);
 		$record->releases = json_decode($record->releases);
 		$record->data = json_decode($record->data);
+
+		foreach($record as $key => $val) {
+			if(!in_array($key, $fields)) {
+				unset($record->{$key});
+			}
+		}
+
 		$json[$idx] = (object) array_replace((array) $json[$idx], (array) $record);
 		array_push($updated, $json[$idx]);
+
 	}
 
 	try {
 		write($json, JSON_FILE);
+		if(errorOutput()->errors) return errorOutput()->errors;
 		return $updated;
 	} catch (Exception $e) {
 		errorLog('UPDATERECORDS_ERROR', $e->getMessage());
@@ -148,6 +181,7 @@ function deleteRecord($index) {
 		global $json;
 
 	if($json === null) fetchJSON();
+	if(errorOutput()->errors) return errorOutput();
 
 	array_splice($json, $index, 1);
 
@@ -184,7 +218,7 @@ function write($json, $file) {
 	if($file === LOCAL_FILE) {
 
 		if(file_put_contents($file, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES))) {
-			return json_decode(file_get_contents($file))	;
+			return json_decode(file_get_contents($file));
 		}
 	}
 
@@ -195,7 +229,7 @@ function write($json, $file) {
 	curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	if($_REQUEST['proxy']) {
+	if(PROXY) {
 		curl_setopt($ch, CURLOPT_PROXY, PROXY_SERVER);
 		curl_setopt($ch, CURLOPT_PROXYPORT, '80');
 	}
@@ -204,6 +238,23 @@ function write($json, $file) {
 	curl_close($ch);
 
 	return $reponse;
+}
+
+function backup() {
+	global $json;
+
+	$file = dirname(LOCAL_FILE) . DIRECTORY_SEPARATOR . 'data.' . date('Ymd') . '.backup.json';
+
+	if($json === null) fetchJSON();
+	if(errorOutput()->errors) return errorOutput();
+
+	if(@file_put_contents($file, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES))) {
+		return json_decode(file_get_contents($file));
+	} else {
+		errorLog('BACKUP_JSON_ERROR', 'Unable to create ' . $file);
+		return errorOutput();
+	}
+
 }
 
 function out($params) {
